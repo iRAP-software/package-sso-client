2e7d03a641f5fadc15f80f65ba69048f56360520
Mark Glover <mark.glover@irap.org>
2017-05-15 10:33:20 +0100

    Adds main package class SsoClient.php

---
+++ SsoClient.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * This client is designed to simplify interactions between brokers and the SSO. 
+ * 
+ * If you require further help, or wish to report a bug fix, please email support@irap.org
+ * 
+ */
+
+namespace iRAP\SsoClient;
+
+require_once __DIR__ . '/bootstrap.php';
+
+class SsoClient
+{
+    
+}
