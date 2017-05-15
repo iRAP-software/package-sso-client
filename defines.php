62b092b1a4928906d16f9dcd5f0d115b395c528d
Mark Glover <mark.glover@irap.org>
2017-05-15 14:17:20 +0100

    Adds defines

--- defines.php
+++ defines.php
@@ -5,7 +5,30 @@
  */
 namespace iRAP\SsoClient;
 
-define(__NAMESPACE__ . '\IRAP_SSO_LIVE_URL', 'http://sso.vida.irap.org');
+# URL for the live SSO
+define(__NAMESPACE__ . '\IRAP_SSO_LIVE_URL', 'https://sso.vida.irap.org');
+
+# Acceptable age for logout requests
+define(__NAMESPACE__ . '\IRAP_SSO_REQUEST_MAX_AGE', 3);
+
+class Settings 
+{
+    # This requires PHP 5.6+
+    # When we move over to PHP 7, we will be able to put this array in a define and
+    # remove the Settings class.
+    const EXPECTED_SSO_LOGIN_PARAMS = array(
+        'user_id',
+        'signature'
+    );
+    
+    # This requires PHP 5.6+
+    # When we move over to PHP 7, we will be able to put this array in a define and
+    # remove the Settings class.
+    const EXPECTED_SSO_LOGOUT_PARAMS = array(
+        'user_id',
+        'signature'
+    );
+}
 
 
 
